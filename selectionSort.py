eb70f50a00ae861b415da12181b176f4f628e140
AnneKay17 <162749912+AnneKay17@users.noreply.github.com>
2026-02-25 21:30:40 +0200

    Add files via upload

---
+++ selectionSort.py
@@ -0,0 +1,17 @@
+class Solution: 
+    def selectionSort(self, arr):
+        #code here
+        size = len(arr)
+        for i in range(size):
+            min_idx = i
+            for j in range(i+1,size):
+                if arr[min_idx] >= arr[j]:
+                    min_idx = j
+            
+            temp = arr[i]
+            arr[i] = arr[min_idx]
+            arr[min_idx] = temp
+            
+        return arr
+
+            
